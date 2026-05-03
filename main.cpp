118fb3a53f781b3f47df466f5fffe80a9bcf3710
Kacper Jakubiak <kajakubiak@student.agh.edu.pl>
2026-05-03 16:58:17 +0200

    zmieniona kolejnosc wczytywania

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <fstream>
+#include <iomanip>
 
 using namespace Eigen;
 using namespace std;
@@ -22,19 +23,27 @@ void save_history(const vector<VectorXd> &history, const string &filename) {
     cout << "Saved to " << path << endl;
 }
 
+void save_matrix(const MatrixXd &matrix, const string &filename) {
+    ofstream out(std::string(PROJECT_ROOT) + "/" + filename);
+    out << std::fixed << std::setprecision(3) << std::setw(5);
+    out << matrix << endl;
+    cout << "Saved " + filename << endl;
+    out.close();
+}
+
 double get_lambda(Index k, Index m, double gamma) {
     if (k < 0 || m <= 0 || I < k)
         throw std::runtime_error("Invalid lambda invocation");
 
     // if (k == 0 || k == I) {
-    //     return pow(m-1, gamma+1) - (m - gamma - 1) * pow(m, gamma);
+    //     return pow(m - 1, gamma + 1) - (m - gamma - 1) * pow(m, gamma);
     // }
 
     return pow(m + 1, gamma + 1) - 2 * pow(m, gamma + 1) + pow(m - 1, gamma + 1);
 }
 
 double get_lambda(Index m) {
-    return 2.0/(1.0 + static_cast<double>(m));
+    return 2.0 / (1.0 + static_cast<double>(m));
 }
 
 MatrixXd calculate_V(const double sign) {
@@ -89,7 +98,7 @@ MatrixXd calculate_R3() {
     MatrixXd R = MatrixXd::Zero(I + 1, I + 1);
     for (Index i = 0; i < I + 1; i++) {
         for (Index k = 0; k <= i; k++) {
-            const double lambda = get_lambda(i-k);
+            const double lambda = get_lambda(i - k);
             R(i, k) += 1.0 * lambda;
             if (k >= 1)
                 R(i, k - 1) += -1.0 * lambda;
@@ -154,44 +163,36 @@ int main(int argc, char *argv[]) {
     double alpha, length, dt, K, mi, theta;
     int J, verbose;
 
-    if (argc < 10) {
-        cout << "Enter: I alpha length dt K mi theta J verbose\n";
-        cin >> I >> alpha >> length >> dt >> K >> mi >> theta >> J >> verbose;
+    if (argc < 9) {
+        cout << "Enter: I length dt K mi theta J verbose alpha\n";
+        cin >> I >> length >> dt >> K >> mi >> theta >> J >> verbose >> alpha;
     } else {
         I = std::stoi(argv[1]);
-        alpha = std::stod(argv[2]);
-        length = std::stod(argv[3]);
-        dt = std::stod(argv[4]);
-        K = std::stod(argv[5]);
-        mi = std::stod(argv[6]);
-        theta = std::stod(argv[7]);
-        J = std::stoi(argv[8]);
-        verbose = std::stoi(argv[9]);
+        length = std::stod(argv[2]);
+        dt = std::stod(argv[3]);
+        K = std::stod(argv[4]);
+        mi = std::stod(argv[5]);
+        theta = std::stod(argv[6]);
+        J = std::stoi(argv[7]);
+        verbose = std::stoi(argv[8]);
+        if (argc == 9) {
+            cout << "Enter alpha: " << endl;
+            cin >> alpha;
+        } else alpha = std::stod(argv[9]);
     }
-    if (verbose > 0) {
-        cout << "I " << I;
-        cout << "\nalpha " << alpha;
-        cout << "\nlength " << length;
-        cout << "\ndt " << dt;
-        cout << "\nK " << K;
-        cout << "\nmi " << mi;
-        cout << "\ntheta " << theta;
-        cout << "\nJ " << J;
-        cout << "\nverbose " << verbose << endl;
-    }
-
+    if (verbose > 0)
+        std::cout << "I " << I << "\nalpha " << alpha << "\nlength " << length << "\ndt " << dt << "\nK " << K <<
+                "\nmi " << mi << "\ntheta " << theta << "\nJ " << J << "\nverbose " << verbose << '\n';
 
     int state;
-    if (std::abs(alpha - 1.0) < 1e-6) {
+    if (std::abs(alpha - 1.0) < 1e-6)
         state = 3;
-    } else if (alpha <= 1.0) {
+    else if (alpha <= 1.0)
         state = 1;
-    } else {
+    else
         state = 2;
-    }
-    if (verbose > 0) {
-        cout << "state: " << state << endl;
-    }
+
+    if (verbose > 0) cout << "state: " << state << endl;
     constexpr double pi = std::numbers::pi;
     double gamma = std::ceil(alpha) - alpha;
     double dx = length / static_cast<double>(I);
@@ -199,33 +200,26 @@ int main(int argc, char *argv[]) {
     double omega;
     if (state != 3) {
         const double denominator = 2.0 * std::cos(pi * alpha / 2.0) * std::tgamma(2.0 + gamma) *
-                           std::pow(dx, alpha);
+                                   std::pow(dx, alpha);
         omega = -K * dt / denominator;
-    }
-    else {
+    } else {
         omega = -K * dt / (pi * dx);
     }
     double ni = mi * dt / (2.0 * dx);
-    if (verbose > 0) {
-        cout << "gamma: " << gamma;
-        cout << "\nomega: " << omega;
-        cout << "\nni: " << ni << endl;
-    }
+
+    if (verbose > 0)
+        cout << "gamma: " << gamma << "\nomega: " << omega << "\nni: " << ni << endl;
 
     VectorXd f = VectorXd::Zero(I + 1);
     f(I / 2) = length / dx;
     const MatrixXd Id = MatrixXd::Identity(I + 1, I + 1);
     const MatrixXd V = calculate_V(ni);
     const MatrixXd R = get_R(state, gamma);
-    const MatrixXd Rt = R.transpose().eval();//calculate_R1t(gamma);
-
-    // cout << V.format(fmt) << endl << endl;
+    const MatrixXd Rt = R.transpose().eval(); //calculate_R1t(gamma);
 
     const MatrixXd A = R + Rt;
     const MatrixXd lhs = Id + theta * (ni * V + omega * A);
     const MatrixXd rhs = Id - (1.0 - theta) * (ni * V + omega * A);
-    // cout << V << endl;
-    // cout << A << endl;
 
     vector<VectorXd> history;
     history.push_back(f);
@@ -240,5 +234,5 @@ int main(int argc, char *argv[]) {
         history.push_back(f);
     }
     cout << f.transpose().format(fmt) << endl;
-    save_history(history, "alfa15.txt");
+    // save_history(history, "alfa15.txt");
 }
